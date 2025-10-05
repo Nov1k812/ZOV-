d1f5e1eab5cfb580ecdcbe6d5953fad0155d656a
semen <semyon.novikov.1102@icloud.com>
2025-10-05 23:30:45 +0600

    Я ебал меня сосали первый раз научился считать

---
+++ KOTLI/src/Medic.java
@@ -0,0 +1,15 @@
+public class Medic {
+    public static void main(String[] args) {
+        /*
+        System.out.printf("go b!");
+        int govno;
+        */
+
+//for (govno=0; govno !=777;govno++){
+//    System.out.println("ваши котлы" + govno);
+//}
+for (int i=1; i<11;i++){              //ебучий цикл
+    System.out.println(i);
+}
+    }
+        }
